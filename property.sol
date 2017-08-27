faa76caa24d79bb7adf74be71b71e453ef3cf14b
marianogadea <mariano.gadea@gmail.com>
2017-08-27 11:48:00 -0300

    uso mapping, pero todavia tengo problemas para borrar propiedades

--- property.sol
+++ property.sol
@@ -3,19 +3,27 @@ pragma solidity ^0.4.0;
 contract propertyMgmt {
     Property[] public properties;
     address public government;
-    mapping (address => uint) public owner;
+    mapping (address => mapping(uint => bytes32)) public owners;
+    uint PropertyID;
 
     // Events allow light clients to react on
     // changes efficiently.
     //event transfer(address from, address to, uint propCode);
 
     struct Property {
-          string street;
+          bytes32 street;
           string description;
           uint value;
           address owner;
     }
+    
 
+    modifier onlyOwner {
+          require (msg.sender == properties[PropertyID].owner);
+          _;
+    }
+
+ 
 
 
     // This is the constructor whose code is
@@ -26,24 +34,61 @@ contract propertyMgmt {
 
 
 
-    function createProperty(string street, string description, uint value, address owner) returns (uint PropertyID)
+    function createProperty(bytes32 street, string description, uint value, address owner) returns (uint PropertyID)
     {
         if (msg.sender != government) return; //only government can assign{
+        
         PropertyID = properties.length++;
         Property storage p = properties[PropertyID];
         p.street = street;
         p.description = description;
         p.value = value;
         p.owner = owner;
-        return PropertyID;
+       // Add to Owners;
+        addPropertyToOwner(owner, street);
+  
     }
 
-    function transferProperty(address newOwner, uint PropertyID)
+    function transferProperty(address newOwner, uint PropertyID) onlyOwner
     {   
-        if (properties[PropertyID].owner != msg.sender) return;
         Property storage p = properties[PropertyID];
+        removePropertyFromOwner(p.owner,p.street);
         p.owner = newOwner;
+        addPropertyToOwner(p.owner,p.street);
+        
+        //remove from owner
+        
+        
     }
+
+
+    function removePropertyFromOwner(address owner, bytes32 street) {
+        uint ownerpropID = 0;
+        while (owners[owner][ownerpropID] != street) {
+              ownerpropID++;
+          }
+        delete owners[owner][ownerpropID];   
+        //while (owners[owner][ownerpropID] > "0x0") { 
+        //    uint ind = ownerpropID+1;
+        //    owners[owner][ownerpropID] = owners[owner][ind];
+        //    ownerpropID++;
+        //}
+        
+    }
+ 
+    function addPropertyToOwner(address owner, bytes32 street) {
+        uint ownerpropID = 0;
+        while (owners[owner][ownerpropID] != "") {
+            ownerpropID++;
+        }
+        owners[owner][ownerpropID] = street;
+    }
+ 
+    //function isStringEmpty(string text) returns (bool isEmpty)
+    //{ bytes memory tempEmptyStringTest = bytes(text); // Uses memory
+    //    isEmpty = false;
+    //    if (tempEmptyStringTest.length == 0) {
+    //        isEmpty = true; 
+    //    } 
+    //}    
 }
- 
- 
